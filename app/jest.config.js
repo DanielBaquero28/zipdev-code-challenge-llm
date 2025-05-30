ada62e2562843fc2eb3cafb411dfd041f0119e8c
DanielBaquero28 <danielalejandrobaquero@hotmail.com>
2025-05-29 21:48:50 -0500

    [Feature]: Added babel packages and config file to transpile JS files.

--- app/jest.config.js
+++ app/jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     setupFiles: ["<rootDir>/jest.setup.js"],
+    transform: {
+    "^.+\\.tsx?$": "ts-jest",        // Transform TypeScript files with ts-jest.
+    "^.+\\.js$": "babel-jest",       // Transform JavaScript files with babel-jest.
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(node-fetch)/)" // Ensure node-fetch is transformed.
+  ],
 };
